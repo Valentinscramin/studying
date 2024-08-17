5db4afaa2796c6a348daa218d50267e461bb65fb
Valentin Oliveira Scremin <valentinscramin@gmail.com>
2024-08-16 23:07:49 -0300

    ADD cart on navbar and showing products based in cookie generated when clicked in add to cart at SHOP

--- resources/js/Pages/Site/Shop.vue
+++ resources/js/Pages/Site/Shop.vue
@@ -1,62 +1,55 @@
 <template>
-    <SiteLayout>
+<SiteLayout>
 
-        <Head title="Loja" />
-        <h1>SHOP</h1>
-        <div class="container">
-            <div class="flex justify-between gap-10">
-                <button class="btn btn-primary" v-for="item in categories" :key="item.id"
-                    @click="selectCategorie(item.id)">{{ item . name }}</button>
-            </div>
-            <hr class="my-4">
-            <div class="flex justify-between gap-10" v-if="isClicked">
-                <div class="card" style="width: 18rem;" v-for="item in data" :key="item.id">
-                    <img :src="'/storage/'+item.img" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ item . name }}</h5>
-                        <p class="card-text">{{ item . description }}</p>
-                        <p class="card-text">{{ item . price }}</p>
-                        <a href="" class="btn btn-primary">Ver Mais</a>
-                    </div>
-                </div>
-            </div>
+    <Head title="Loja" />
+    <h1>SHOP</h1>
+    <div class="container">
+        <div class="flex justify-between gap-10">
+            <button class="btn btn-primary" v-for="item in categories" :key="item.id" @click="selectCategorie(item.id)">{{ item . name }}</button>
         </div>
-    </SiteLayout>
+        <hr class="my-4">
+        <div class="flex justify-between gap-10">
+            <CardProduct :data="data" v-if="isClicked"/>
+        </div>
+    </div>
+</SiteLayout>
 </template>
+
 <script setup>
-    import SiteLayout from '@/Layouts/SiteLayout.vue';
-    import {
-        Head,
-    } from '@inertiajs/vue3';
-    import {
-        ref
-    } from 'vue';
-    import axios from 'axios';
+import SiteLayout from '@/Layouts/SiteLayout.vue';
+import {
+    Head,
+} from '@inertiajs/vue3';
+import {
+    ref
+} from 'vue';
+import axios from 'axios';
+import CardProduct from '@/Components/CardProduct.vue';
 
-    const props = defineProps({
-        categories: Object
-    });
+const props = defineProps({
+    categories: Object
+});
 
-    const isClicked = ref(false);
-    const lastCategory = ref(0);
-    const data = ref();
-    const error = ref();
-    const loading = ref();
+const isClicked = ref(false);
+const lastCategory = ref(0);
+const data = ref();
+const error = ref();
+const loading = ref();
 
-    const selectCategorie = async (categorieId) => {
-        if (lastCategory.value != categorieId) {
-            isClicked.value = true;
-            lastCategory.value = categorieId;
+const selectCategorie = async (categorieId) => {
+    if (lastCategory.value != categorieId) {
+        isClicked.value = true;
+        lastCategory.value = categorieId;
 
-            try {
-                const response = await axios.get('http://127.0.0.1:8000/api/products/categories/' + categorieId)
-                data.value = response.data;
-            } catch (err) {
-                error.value = 'Falha ao carregar os dados'
-            } finally {
-                loading.value = false
-            }
-
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/products/categories/' + categorieId)
+            data.value = response.data;
+        } catch (err) {
+            error.value = 'Falha ao carregar os dados'
+        } finally {
+            loading.value = false
         }
+
     }
+}
 </script>
